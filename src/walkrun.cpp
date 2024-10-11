e7aa329ca42e9181cced3fea60ad5b47355bedb2
Joe Bentley <joebentley10@gmail.com>
2024-10-11 15:49:08 +0200

    Fixed more formatting

--- src/walkrun.cpp
+++ src/walkrun.cpp
@@ -77,121 +77,121 @@ int main(int argc, char *argv[])
 
     // Use 3D lattice
     if (simplecubic || hexagonal) {
-	Lattice<3> lattice;
+        Lattice<3> lattice;
 
-	if (simplecubic)
-	    lattice = SimpleCubic();
-	else if (hexagonal)
-	    lattice = Hexagonal();
+        if (simplecubic)
+            lattice = SimpleCubic();
+        else if (hexagonal)
+            lattice = Hexagonal();
 
-	/* Generate the random walk, applying the basis set */
-	Walk<3> random_walk = Walk<3>(lattice).generate(walk_length).applyBasis();
+        /* Generate the random walk, applying the basis set */
+        Walk<3> random_walk = Walk<3>(lattice).generate(walk_length).applyBasis();
 
-	// TODO: Currently just copied from below...move it
-	// Calculate and print the distance between the start and end point of the walk
-	if (distance) {
-	    // invariant: i random walk distances have been calculated
-	    for (int i = 0; i < distance_count; ++i) {
-		int distance = random_walk.getDistance();
+        // TODO: Currently just copied from below...move it
+        // Calculate and print the distance between the start and end point of the walk
+        if (distance) {
+            // invariant: i random walk distances have been calculated
+            for (int i = 0; i < distance_count; ++i) {
+            int distance = random_walk.getDistance();
 
-		if (!suppress_output)
-		    std::cout << distance << std::endl;
+            if (!suppress_output)
+                std::cout << distance << std::endl;
+
+                random_walk.generate(walk_length).applyBasis();
+            }
+
+        // Accumulate the vectors at each step
+        } else if (accumulate) {
+            if (!suppress_output)
+            std::cout << random_walk.accumulateVectors().toCSV() << std::endl;
+        // Print out the vectors without accumulating them
+        } else {
+            if (!suppress_output)
+            std::cout << random_walk.toCSV() << std::endl;
+        }
+
+        return 0;
+    } else {
+        /* Use a 2D lattice */
+        Lattice<2> lattice;
+
+        if (square) {
+            lattice = SquareLattice();
+        } else {
+            lattice = TriLattice();
+        }
+
+        // Generate a diffusion limited aggregation
+        if (pointDLA) {
+            Walk<2> walk(lattice);
+            PointDLA dla(stickiness);
+
+            /* Generate until user manually stops it */
+            for (;;) {
+                if (fractal_dimension) {
+                    dla.simulateInRadius(walk);
+
+                    // Output N vs. R
+                    auto N = dla.getSeeds().size();
+                    double R = dla.getFurthestRadius();
+
+                    if (!suppress_output)
+                    std::cout << N << ", " << R << std::endl;
+                } else {
+                    Vector<2> point = dla.simulateInRadius(walk);
+
+                    if (!suppress_output)
+                        std::cout << point << std::endl;
+                }
+            }
+
+            return 0;
+        }
+
+        if (lineDLA) {
+            Walk<2> walk(lattice);
+
+            LineDLA dla(line_width, stickiness);
+
+            // Output the initial seeds (TODO: Don't do this here...)
+            for (int x = -line_width; x <= line_width; ++x) {
+            if (!suppress_output)
+                std::cout << Vector<2>(2, (double)x, 0.0) << std::endl;
+            }
+
+            /* Generate until user manually stops it */
+            for (;;) {
+            Vector<2> point = dla.simulate(walk);
+            if (!suppress_output)
+                std::cout << point << std::endl;
+            }
+
+            return 0;
+        }
+
+        /* Generate the random walk, applying the basis set */
+        Walk<2> random_walk = Walk<2>(lattice).generate(walk_length).applyBasis();
+
+        // Calculate and print the distance between the start and end point of the walk
+        if (distance) {
+            // invariant: i random walk distances have been calculated
+            for (int i = 0; i < distance_count; ++i) {
+            int distance = random_walk.getDistance();
+
+            if (!suppress_output)
+                std::cout << distance << std::endl;
 
             random_walk.generate(walk_length).applyBasis();
-	    }
-
-	// Accumulate the vectors at each step
-	} else if (accumulate) {
-	    if (!suppress_output)
-		std::cout << random_walk.accumulateVectors().toCSV() << std::endl;
-	// Print out the vectors without accumulating them
-	} else {
-	    if (!suppress_output)
-		std::cout << random_walk.toCSV() << std::endl;
-	}
-
-	return 0;
-    } else {
-	/* Use a 2D lattice */
-	Lattice<2> lattice;
-
-	if (square) {
-	    lattice = SquareLattice();
-	} else {
-	    lattice = TriLattice();
-	}
-
-	// Generate a diffusion limited aggregation
-	if (pointDLA) {
-	    Walk<2> walk(lattice);
-	    PointDLA dla(stickiness);
-
-	    /* Generate until user manually stops it */
-	    for (;;) {
-		if (fractal_dimension) {
-		    dla.simulateInRadius(walk);
-
-		    // Output N vs. R
-		    auto N = dla.getSeeds().size();
-		    double R = dla.getFurthestRadius();
-
-		    if (!suppress_output)
-			std::cout << N << ", " << R << std::endl;
-		} else {
-		    Vector<2> point = dla.simulateInRadius(walk);
-
-		    if (!suppress_output)
-			std::cout << point << std::endl;
-		}
-	    }
-
-	    return 0;
-	}
-
-	if (lineDLA) {
-	    Walk<2> walk(lattice);
-
-	    LineDLA dla(line_width, stickiness);
-
-	    // Output the initial seeds (TODO: Don't do this here...)
-	    for (int x = -line_width; x <= line_width; ++x) {
-		if (!suppress_output)
-		    std::cout << Vector<2>(2, (double)x, 0.0) << std::endl;
-	    }
-
-	    /* Generate until user manually stops it */
-	    for (;;) {
-		Vector<2> point = dla.simulate(walk);
-		if (!suppress_output)
-		    std::cout << point << std::endl;
-	    }
-
-	    return 0;
-	}
-
-	/* Generate the random walk, applying the basis set */
-	Walk<2> random_walk = Walk<2>(lattice).generate(walk_length).applyBasis();
-
-	// Calculate and print the distance between the start and end point of the walk
-	if (distance) {
-	    // invariant: i random walk distances have been calculated
-	    for (int i = 0; i < distance_count; ++i) {
-		int distance = random_walk.getDistance();
-
-		if (!suppress_output)
-		    std::cout << distance << std::endl;
-
-		random_walk.generate(walk_length).applyBasis();
-	    }
-	// Accumulate the vectors at each step
-	} else if (accumulate) {
-	    if (!suppress_output)
-		std::cout << random_walk.accumulateVectors().toCSV() << std::endl;
-	// Print out the vectors without accumulating them
-	} else {
-	    if (!suppress_output)
-		std::cout << random_walk.toCSV() << std::endl;
-	}
+            }
+        // Accumulate the vectors at each step
+        } else if (accumulate) {
+            if (!suppress_output)
+            std::cout << random_walk.accumulateVectors().toCSV() << std::endl;
+        // Print out the vectors without accumulating them
+        } else {
+            if (!suppress_output)
+            std::cout << random_walk.toCSV() << std::endl;
+        }
     }
 
     return 0;
